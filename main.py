e7b4f84f99656f8702aa3640b2a8b77767f4e25f
rahulkishne3 <99482503+rahulkishne3@users.noreply.github.com>
2022-02-11 17:51:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,176 @@
+# All media file is available for download as a zip file
+import numpy as np
+import os
+import tkinter 
+import cv2 # pip install opencv-python
+import PIL.Image, PIL.ImageTk # pip install pillow
+from functools import partial
+import threading
+import time
+import imutils # pip install imutils
+
+stream = cv2.VideoCapture("video.avi")
+
+filename = 'video.avi'
+frames_per_seconds = 24.0
+my_res = '360p'
+
+
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+
+STD_DIMENSION = {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+    "4k": (3840, 2160),
+}
+
+
+def get_dims(cap, res='1080'):
+    width, height = STD_DIMENSION['480p']
+    if res in STD_DIMENSION:
+        width, height = STD_DIMENSION[res]
+    change_res(cap, width, height)
+    return width, height
+
+VIDEO_TYPE = {
+    'avi': cv2.VideoWriter_fourcc(*'XVID'),
+    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+}
+
+def get_video_type(filename):
+    filename, ext = os.path.splitext(filename)
+    if ext in VIDEO_TYPE:
+        return VIDEO_TYPE[ext]
+    return VIDEO_TYPE['avi']
+
+
+
+cap = cv2.VideoCapture(0)
+dims = get_dims(cap, res=my_res)
+video_type_cv2 = get_video_type(filename)
+
+out = cv2.VideoWriter(filename, video_type_cv2, frames_per_seconds, dims)
+
+
+while True:
+
+    check, frame = cap.read()
+
+    out.write(frame)
+    print(check)
+    print(frame)
+
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Capturing", frame)
+
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+flag = True
+def play(speed):
+    global flag
+    print("You clicked on play. Speed is {speed}")
+
+    # Play the video in reverse mode
+    
+    frame1 = stream.get(cv2.CAP_PROP_POS_FRAMES)
+    stream.set(cv2.CAP_PROP_POS_FRAMES, frame1 + speed)
+
+    grabbed, frame = stream.read()
+    
+    if not grabbed:
+        exit()
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+    if flag:
+        canvas.create_text(134, 26, fill="black", font="Times 26 bold", text="Decision Pending")
+    flag = not flag
+    
+
+def pending(decision):
+    # 1. Display decision pending image
+    frame = cv2.cvtColor(cv2.imread("pending.png"), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+    # 2. Wait for 1 second
+    time.sleep(1.5)
+
+    # 3. Display sponsor image
+    frame = cv2.cvtColor(cv2.imread("sponsor.png"), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+
+    # 4. Wait for 1.5 second
+    time.sleep(2.5)
+    # 5. Display out/notout image
+    if decision == 'out':
+        decisionImg = "out.png"
+    else:
+        decisionImg = "not_out.png"
+    frame = cv2.cvtColor(cv2.imread(decisionImg), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+
+
+def out():
+    thread = threading.Thread(target=pending, args=("out",))
+    thread.daemon = 1
+    thread.start()
+    print("Player is out")
+
+
+def not_out():
+    thread = threading.Thread(target=pending, args=("not out",))
+    thread.daemon = 1
+    thread.start()
+    print("Player is not out")
+
+# Width and height of our main screen
+SET_WIDTH = 650
+SET_HEIGHT = 368
+
+# Tkinter gui starts here
+window = tkinter.Tk()
+window.title("CodeWithHarry Third Umpire Decision Review Kit")
+cv_img = cv2.cvtColor(cv2.imread("welcome.png"), cv2.COLOR_BGR2RGB)
+canvas = tkinter.Canvas(window, width=SET_WIDTH, height=SET_HEIGHT)
+photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
+image_on_canvas = canvas.create_image(0, 0, ancho=tkinter.NW, image=photo)
+canvas.pack()
+
+
+# Buttons to control playback
+btn = tkinter.Button(window, text="<< Previous (fast)", width=50, command=partial(play, -25))
+btn.pack()
+
+btn = tkinter.Button(window, text="<< Previous (slow)", width=50, command=partial(play, -2))
+btn.pack()
+
+btn = tkinter.Button(window, text="Next (slow) >>", width=50, command=partial(play, 2))
+btn.pack()
+
+btn = tkinter.Button(window, text="Next (fast) >>", width=50, command=partial(play, 25))
+btn.pack()
+
+btn = tkinter.Button(window, text="Give Out", width=50, command=out)
+btn.pack()
+
+btn = tkinter.Button(window, text="Give Not Out", width=50, command=not_out)
+btn.pack()
+window.mainloop()
